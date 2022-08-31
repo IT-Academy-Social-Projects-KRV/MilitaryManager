d3af8314d950d26bb2f3094dca9c09fd713d8dd8
Ponomarov Oleksii <testdrive819@gmail.com>
2022-08-31 21:24:50 +0300

    implemented first test. not working for now

--- DocumentGenerator.Program/Program.cs
+++ DocumentGenerator.Program/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using BusinessLogic.Services.Documents;
 
 namespace DocumentGenerator.Program
 {
+    //"G:\\CSHARP\\SoftServe\\MilitaryManager\\MilitaryManager.Attachments.API\\wwwroot\\data\\document_templates\\template_01.xml"
     class Program
     {
         static void Main(string[] args)
         {
+            IDocumentGenerationService documentGenerationService = new DocumentGenerationService();
+
+            string exportPath = "G:\\pdfs\\";
+            string templateName = "template_01";
+            string templateData = "template_01.xml";
+            string jsonData = "template_01.json";
 
 
+
+            documentGenerationService.GeneratePdfDocument(exportPath, templateName, templateData, jsonData);
+
+            Console.WriteLine("press enter please");
             Console.ReadLine();
         }
     }
