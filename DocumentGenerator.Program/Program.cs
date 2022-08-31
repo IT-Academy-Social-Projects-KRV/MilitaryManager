e47b6c3638fea69c55f67f88df4173667b308dcc
Ponomarov Oleksii <testdrive819@gmail.com>
2022-08-31 20:45:21 +0300

    add DocumentGenerator.Program project

---
+++ DocumentGenerator.Program/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DocumentGenerator.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+    }
+}
