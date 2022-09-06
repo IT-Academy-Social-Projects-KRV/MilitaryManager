37f52d6dfb4a9cd731b2601ce3b3e9f6b00454eb
Angela <70724933+mershalova@users.noreply.github.com>
2022-09-06 14:09:17 +0300

    Update app-routing.module.ts

--- MilitaryManager/client-app/src/app/app-routing.module.ts
+++ MilitaryManager/client-app/src/app/app-routing.module.ts
@@ -5,14 +5,6 @@ import { Test2Component } from './components/test2/test2.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppLayoutComponent } from './layout/app.layout.component';
 
-// const routes: Routes = [
-//     { path: '', redirectTo: 'home', pathMatch: 'full' },
-//     { path: 'home', component: HomeComponent },
-//     { path: 'test1', component: Test1Component },
-//     { path: 'test2', component: Test2Component },
-//     { path: 'units', loadChildren: () => import("./modules/units-module/units.module").then(m => m.UnitsModule) },
-//     { path: '**', redirectTo: 'home' }
-// ];
 const routes: Routes = [
     {path:'', component:AppLayoutComponent,
   children:[
