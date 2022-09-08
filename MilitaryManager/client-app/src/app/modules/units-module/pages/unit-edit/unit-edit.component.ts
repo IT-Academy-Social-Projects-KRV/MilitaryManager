e3affba8e5db52203b6183f2803ae711f249b30d
Natalie <90128567+NataliaHerus@users.noreply.github.com>
2022-09-08 17:59:25 +0300

    Feat/2/Statement Managment (#5)
    * Add services to implement statement managment
    
    * Add line breaks
    
    * Remove useless method getAll
    
    * Resolved merge conflict

--- MilitaryManager/client-app/src/app/modules/units-module/pages/unit-edit/unit-edit.component.ts
+++ MilitaryManager/client-app/src/app/modules/units-module/pages/unit-edit/unit-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UnitStateService } from 'src/app/shared/services/unit-state.service';
 
 @Component({
   selector: 'app-unit-edit',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnitEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private unitsService: UnitStateService) { }
 
   ngOnInit(): void {
   }
--- MilitaryManager/client-app/src/app/shared/services/api/unit.service.ts
+++ MilitaryManager/client-app/src/app/shared/services/api/unit.service.ts
@@ -4,12 +4,17 @@ import { HttpService } from "../core/http.service";
 import { BaseService } from "../core/base.service";
 import { ClientConfigurationService } from "../core/client-configuration.service";
 import { ServiceType } from "../core/serviceType";
+import {Observable } from "rxjs";
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class UnitsService extends BaseService<any> {
+  
   constructor(
     httpService: HttpService,
     configService: ClientConfigurationService) {
     super(httpService, 'weatherForecast', configService, UnitModel, ServiceType.units);
   }
+
 }
