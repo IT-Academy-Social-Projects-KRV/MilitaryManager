ba299490c5e1424e627ec47bc4bfebc7a7e6841e
Alex Rozdobudko <arozdobudko2@gmail.com>
2022-09-12 20:39:28 +0300

    rebase

---
+++ MilitaryManager.Core/Entities/UnitEntity/Unit.cs
@@ -0,0 +1,17 @@
+﻿using MilitaryManager.Core.Interfaces;
+using System.Collections.Generic;
+
+namespace MilitaryManager.Core.Entities.UnitEntity
+{
+    public class Unit : IBaseEntity<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int? ParentId { get; set; }
+
+        public virtual Unit Parent { get; set; }
+
+        public virtual ICollection<Unit> SubUnits { get; set; }
+    }
+}
---
+++ MilitaryManager.Core/Interfaces/Services/IUnitService.cs
@@ -0,0 +1,16 @@
+﻿using MilitaryManager.Core.DTO.Units;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MilitaryManager.Core.Interfaces.Services
+{
+    public interface IUnitService
+    {
+        Task<IEnumerable<UnitDTO>> GetUnitsTreeAsync();
+        Task<IEnumerable<UnitDTO>> GetUnitsAsync();
+        Task<UnitDTO> GetUnitByIdAsync(int id);
+        Task CreateUnitAsync(UnitDTO query);
+        Task UpdateUnitAsync(UnitDTO query);
+        Task DeleteUnitAsync(UnitDTO query);
+    }
+}
