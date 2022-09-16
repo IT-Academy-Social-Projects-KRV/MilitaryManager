198ecd258c3452036c0c0289e32a9113303bc7b1
Ponomarov Oleksii <testdrive819@gmail.com>
2022-09-16 10:53:04 +0300

    add mock data for json

---
+++ MilitaryManager.Attachments.API/Data/MilitaryUnit.cs
@@ -0,0 +1,10 @@
+﻿namespace MilitaryManager.Attachments.API.Data
+{
+    public class MilitaryUnit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        
+    }
+}
