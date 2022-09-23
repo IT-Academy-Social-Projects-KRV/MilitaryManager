159678376d2707077ac896d60271aea90564c699
Artur Hostiuk <artur1100011@gmail.com>
2022-09-23 17:52:06 +0300

    remove unnecessary usings

--- MilitaryManager.Units.API/Startup.cs
+++ MilitaryManager.Units.API/Startup.cs
@@ -1,19 +1,11 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Logging;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using MilitaryManager.Core;
 using MilitaryManager.Infrastructure;
 
@@ -31,8 +23,6 @@ namespace MilitaryManager.Units.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                .AddJwtBearer(opt =>
                {
