6f49e5a39e48d6d421d7c2aada8017d8c9cb2d2c
Alex Rozdobudko <arozdobudko2@gmail.com>
2022-10-03 11:29:52 +0300

    Add swagger for Unit.Api

--- MilitaryManager.Core/DTO/Units/UnitDTO.cs
+++ MilitaryManager.Core/DTO/Units/UnitDTO.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using MilitaryManager.Core.Entities.DivisionEntity;
+using MilitaryManager.Core.Entities.PositionEntity;
+using MilitaryManager.Core.Entities.RankEntity;
+using MilitaryManager.Core.Entities.UnitEntity;
+using System.Collections.Generic;
 
 namespace MilitaryManager.Core.DTO.Units
 {
     public class UnitDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int? DivisionId { get; set; }
+        public int RankId { get; set; }
         public int? ParentId { get; set; }
+        public int PositionId { get; set; }
 
-        public UnitDTO Parent { get; set; }
+        public Division Division { get; set; }
+        public Rank Rank { get; set; }
+        public Unit Parent { get; set; }
+        public Position Position { get; set; }
 
-        public List<UnitDTO> SubUnits { get; set; }
+        public List<Unit> SubUnits { get; set; }
     }
 }
--- MilitaryManager/client-app/src/environments/environment.prod.ts
+++ MilitaryManager/client-app/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   webserverApiPath: '/api/',
   webUrl: 'https://localhost:5001',
-  unitsApiUrl: 'https://localhost:5005'
+  unitsApiUrl: 'https://localhost:5005',
   identityUrl: 'https://localhost:5007'
 };
