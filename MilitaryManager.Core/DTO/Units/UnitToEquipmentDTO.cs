bc9ec5445a2a35316a4e23966af645561381bcf6
mershalova <angelamersh@gmail.com>
2022-10-05 20:06:46 +0300

    Added DTOs

---
+++ MilitaryManager.Core/DTO/Units/UnitToEquipmentDTO.cs
@@ -0,0 +1,23 @@
+﻿using MilitaryManager.Core.Entities.DivisionEntity;
+using MilitaryManager.Core.Entities.EntityEntity;
+using MilitaryManager.Core.Entities.UnitEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryManager.Core.DTO.Units
+{
+    public class UnitToEquipmentDTO
+    {
+        public int Id { get; set; }
+        public int? UnitId { get; set; }
+        public int GivenById { get; set; }
+        public int DivisionId { get; set; }
+        public DateTime GivenDate { get; set; }
+
+        //public DivisionDTO Division { get; set; }
+        public EntityDTO Equipment { get; set; }
+        public UnitDTO Unit { get; set; }
+        public UnitDTO Warehouseman { get; set; }
+    }
+}
