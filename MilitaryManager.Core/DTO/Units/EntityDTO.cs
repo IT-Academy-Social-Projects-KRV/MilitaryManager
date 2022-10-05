7b433966880576c9f43f44a01af034d02c81f64e
mershalova <angelamersh@gmail.com>
2022-10-05 20:06:46 +0300

    Added DTOs

---
+++ MilitaryManager.Core/DTO/Units/EntityDTO.cs
@@ -0,0 +1,17 @@
+﻿using MilitaryManager.Core.Entities.EntityToAttributeEntity;
+using MilitaryManager.Core.Entities.EquipmentToUnitEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryManager.Core.DTO.Units
+{
+    public class EntityDTO
+    {
+        public int Id { get; set; }
+        public string RegNum { get; set; }
+
+        public List<EntityToAttributeDTO> EntityToAttributes { get; set; }
+        public UnitToEquipmentDTO UnitToEquipment { get; set; }
+    }
+}
---
+++ MilitaryManager.Core/DTO/Units/RankDTO.cs
@@ -0,0 +1,15 @@
+﻿using MilitaryManager.Core.Entities.UnitEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryManager.Core.DTO.Units
+{
+    public class RankDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<UnitDTO> Units { get; set; }
+    }
+}
