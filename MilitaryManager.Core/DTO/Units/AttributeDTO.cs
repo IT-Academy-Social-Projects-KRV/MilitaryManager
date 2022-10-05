3cef717a378e1c8c386b980143235137c7375865
mershalova <angelamersh@gmail.com>
2022-10-05 20:06:46 +0300

    Added DTOs

---
+++ MilitaryManager.Core/DTO/Units/AttributeDTO.cs
@@ -0,0 +1,16 @@
+﻿using MilitaryManager.Core.Entities.EntityToAttributeEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryManager.Core.DTO.Units
+{
+    public class AttributeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<ProfileDTO> Profiles { get; set; }
+        public List<EntityToAttributeDTO> EntityToAttributes { get; set; }
+    }
+}
---
+++ MilitaryManager.Core/DTO/Units/EntityToAttributeDTO.cs
@@ -0,0 +1,18 @@
+﻿using MilitaryManager.Core.Entities.EntityEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryManager.Core.DTO.Units
+{
+    public class EntityToAttributeDTO
+    {
+        public int Id { get; set; }
+        public int EntityId { get; set; }
+        public int AttributeId { get; set; }
+        public string Value { get; set; }
+
+        public EntityDTO Entity { get; set; }
+        public AttributeDTO Attribute { get; set; }
+    }
+}
