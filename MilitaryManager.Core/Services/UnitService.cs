936936b730d3e2b48b6d662d8c37871b86d6c926
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2022-10-07 23:52:04 +0300

    Very small refactor

--- MilitaryManager.Core/Services/UnitService.cs
+++ MilitaryManager.Core/Services/UnitService.cs
@@ -5,7 +5,6 @@ using MilitaryManager.Core.Entities.UnitEntity;
 using MilitaryManager.Core.Interfaces.Repositories;
 using MilitaryManager.Core.Interfaces.Services;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace MilitaryManager.Core.Services
