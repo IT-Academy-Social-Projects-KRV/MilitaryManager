279946e305c3ed8e817b469f73749b825dfb9eb5
mershalova <angelamersh@gmail.com>
2022-10-10 00:34:45 +0300

    test layout for finish registration

--- MilitaryManager/client-app/src/app/components/finish-registration/finish-registration.component.ts
+++ MilitaryManager/client-app/src/app/components/finish-registration/finish-registration.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinishRegistrationComponent implements OnInit {
 
+  firstname?: string;
+  lastname?: string;
+  middlename?: string;
+  position?: string;
+  rank?: string;
+
+  divisions: string[] = [
+    'division1',
+    'division2',
+    'division3'
+  ];
+
+  selected_division?: string;
+
   constructor() { }
 
   ngOnInit(): void {
