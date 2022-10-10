3d326cf38b1242a446a4c56cb4878441857b5d93
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2022-10-10 10:30:42 +0300

    Seeding data in separate method

--- MilitaryManager.Infrastructure/Data/MilitaryManagerDbContext.cs
+++ MilitaryManager.Infrastructure/Data/MilitaryManagerDbContext.cs
@@ -46,7 +46,6 @@ namespace MilitaryManager.Infrastructure.Data
             modelBuilder.ApplyConfiguration(new StatusHistoryConfiguration());
             modelBuilder.ApplyConfiguration(new TemplateConfiguration());
             modelBuilder.ApplyConfiguration(new SignedPdfConfiguration());
-            modelBuilder.Seed();
             modelBuilder.ApplyConfiguration(new ChangeConfiguration());
             modelBuilder.ApplyConfiguration(new ChangeTypeConfiguration());
             modelBuilder.ApplyConfiguration(new ChangeValueConfiguration());
@@ -59,41 +58,8 @@ namespace MilitaryManager.Infrastructure.Data
 
                 entity.Property(x => x.OldValue).HasColumnType("sql_variant");
             });
-            
-            modelBuilder.Entity<ChangeType>().HasData(
-                new ChangeType[]
-                {
-                    new ChangeType { Code = 'I', Name = "Insert"},
-                    new ChangeType { Code = 'U', Name = "Update"},
-                    new ChangeType { Code = 'D', Name = "Delete"}
-                });
 
-            modelBuilder.Entity<Table>().HasData(
-                new Table[]
-                {
-                    new Table { Name = "Attributes", Description = "Attributes for units and divisions"},
-                    new Table { Name = "Divisions", Description = "Information about divisions"},
-                    new Table { Name = "Entities", Description = "List of equipments"},
-                    new Table { Name = "EntityToAttributes", Description = "Decoupling table for the connection between equipment and its attributes"},
-                    new Table { Name = "Positions", Description = "List of unit positions"},
-                    new Table { Name = "Profiles", Description = "Decoupling table for the connection between unit and its attributes"},
-                    new Table { Name = "Ranks", Description = "List of unit ranks"},
-                    new Table { Name = "Units", Description = "Information about unit"},
-                    new Table { Name = "UnitToEquipments", Description = "Decoupling table for the connection between unit and its equipment"}
-                });
-            modelBuilder.Entity<Column>().HasData(
-                new Column[]
-                {
-                    new Column { Name = "UnitParentId", TableName = "Units"},
-                    new Column { Name = "DivisionsId", TableName = "Units"},
-                    new Column { Name = "FirstName", TableName = "Units"},
-                    new Column { Name = "LastName", TableName = "Units"},
-                    new Column { Name = "PositionsId", TableName = "Units"},
-                    new Column { Name = "RankId", TableName = "Units"},
-                    new Column { Name = "Name", TableName = "Divisions"},
-                    new Column { Name = "Address", TableName = "Divisions"},
-                    new Column { Name = "DivisionParentId", TableName = "Divisions"}
-                });
+            modelBuilder.Seed();
         }
 
         public DbSet<Attribute> Attributes { get; set; }
