0a03b4a8ff557f2ba5450a1bf882792e3d77e2d1
Natalia Herus <natalia.gerus892@gmail.com>
2022-10-11 00:02:24 +0300

    fix data, fix drop-down

--- MilitaryManager/client-app/src/app/modules/attachments-module/pages/decree-add/decree-add.component.ts
+++ MilitaryManager/client-app/src/app/modules/attachments-module/pages/decree-add/decree-add.component.ts
@@ -3,7 +3,6 @@ import { AttachmentModel } from '../../../../shared/models/attachment.model'
 import { UnitModel } from 'src/app/shared/models/unit.model';
 import { MessageService } from 'primeng/api';
 import { DecreeService } from 'src/app/shared/services/api/decree.service';
-import { ApiService } from 'src/app/shared/services/api/api.service';
 import { UnitsService } from 'src/app/shared/services/api/unit.service';
 
 @Component({
@@ -27,18 +26,21 @@ export class DecreeAddComponent implements OnInit {
 
     addDecree(attachment: AttachmentModel[]) {
       let decreeInformation;
+      let dateFormatter = new Intl.DateTimeFormat('uk-UA');
+
       for(const element of attachment)
       {
         decreeInformation = 
-        { templateId: this.templateId, 
-          decreeName: `${this.templateType} від ${element.currentDate?.toLocaleDateString("ro-Ro")}`,
+        { 
+          templateId: this.templateId, 
+          decreeName: `${this.templateType} від ${dateFormatter.format(new Date(element.currentDate!))}`,
           lastName: element.soldier?.lastName,
           firstName: element.soldier?.firstName,
-          currentDate: element.currentDate,
+          currentDate: dateFormatter.format(new Date(element.currentDate!)),
           unitNumber: element.soldier?.divisionId
         };
+        this.decreeService.single.create(decreeInformation).subscribe({});
       }
-      this.decreeService.single.create(decreeInformation).subscribe({});
     }
 
     addSoldier(soldier: UnitModel) {
@@ -49,7 +51,6 @@ export class DecreeAddComponent implements OnInit {
         newDecree.payOff = false;
         newDecree.action = this.templateType;
         newDecree.currentDate = new Date();
-        console.log(newDecree);
         this.decrees.push(newDecree);
       }
   }
@@ -60,6 +61,7 @@ export class DecreeAddComponent implements OnInit {
       editAttachment.action = attachment.action
       editAttachment.currentDate = attachment.currentDate
       editAttachment.payOff= attachment.payOff;
+
       if(editAttachment.id!=null)
       {
         this.clonedDecrees[editAttachment.id]=editAttachment;
@@ -93,16 +95,14 @@ export class DecreeAddComponent implements OnInit {
       setTimeout(() => {
         this.unitsService.collection.getAll()
           .subscribe((units) => {
-            this.units = units;
-        });
+            this.units = units.map((unit: any) => {
+              return {
+                ...unit,
+                displayLabel: unit.lastName + ' ' + unit.firstName + ' ' ,
+              };
+            });
       });
-      this.units = this.units.map((unit: any) => {
-        return {
-          ...unit,
-          displayLabel: unit.lastName + ' ' + unit.firstName + ' ' + unit.secondName,
-        };
       });
-    }
-
+  }
 }
 
