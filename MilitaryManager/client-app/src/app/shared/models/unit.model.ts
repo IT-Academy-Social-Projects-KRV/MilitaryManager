3e64632313c636c951c82a3a9bb92c86ee96bbb1
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2022-10-13 09:21:46 +0300

    Deleted comments

--- MilitaryManager/client-app/src/app/shared/models/unit.model.ts
+++ MilitaryManager/client-app/src/app/shared/models/unit.model.ts
@@ -1,13 +1,6 @@
 import { BaseModel } from "./base.model";
 
 export class UnitModel extends BaseModel {
-  // public lastName: string | null = null;
-  // public firstName: string | null = null;
-  // public divisionId: number | null = null;
-  // public rankId: number | null = null;
-  // public positionId: number | null = null;
-  // public parentId: number | null = null;
-
   public parent: UnitModel | null = null;
 
   public subUnits: UnitModel[] | null = null;
