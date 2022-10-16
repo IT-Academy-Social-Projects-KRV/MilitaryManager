1795ea9aad1e5da503d606c38061b3754135d654
Zhykovskiy <68905616+Zhykovskiy@users.noreply.github.com>
2022-10-16 17:26:02 +0000

    Update Attribute.cs

--- MilitaryManager.Core/Entities/AttributeEntity/Attribute.cs
+++ MilitaryManager.Core/Entities/AttributeEntity/Attribute.cs
@@ -11,6 +11,7 @@ namespace MilitaryManager.Core.Entities.AttributeEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        
         public ICollection<Profile> Profiles { get; set; }
         public ICollection<EntityToAttribute> EntityToAttributes { get; set; }
     }
