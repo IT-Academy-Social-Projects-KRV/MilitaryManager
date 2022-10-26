2bfdc99cb1390158aaa0c7b811b841358681faa1
mershalova <angelamersh@gmail.com>
2022-10-26 19:43:56 +0300

    save

---
+++ MilitaryManager/client-app/src/app/modules/equipment/equipment-list/equipment-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, ComponentRef, OnInit, QueryList, ViewContainerRef } from '@angular/core';
+import { EntityModel } from 'src/app/shared/models/entity.model';
+import { EquipmentService } from 'src/app/shared/services/api/equipment.service';
+import {FieldsetModule} from 'primeng/fieldset';
+import { UnitModel } from 'src/app/shared/models/unit.model';
+
+@Component({
+  selector: 'app-equipment-list',
+  templateUrl: './equipment-list.component.html',
+  styleUrls: ['./equipment-list.component.scss']
+})
+export class EquipmentListComponent implements OnInit {
+  empty : string = "немає даних"
+
+  currentTab: number = 0;
+  tabs: EntityModel[] = [];
+  equipment: EntityModel[]=[]
+  cols: any[] = [];
+  fullNames:string[] = [];
+
+  constructor(public equipmentService: EquipmentService) { }
+
+  ngOnInit(): void {
+    this.cols = [
+      { field: 'regNum', header: 'Реєстраційний номер' },
+      { field: 'soldier', header: 'Надано до' },
+      { field: 'division', header: 'Частина' },
+      { field: 'date', header: 'Дата видачі' },
+      { field: 'buttons', header: '', width: '5%'}
+    ];
+    this.equipmentService.collection.getAll().subscribe(res => {this.equipment = res})
+  }
+
+  handleClose(e: any) {
+    this.tabs.splice(e.index - 2, 1);
+    if(this.currentTab == e.index)
+      this.currentTab = e.index - 1;
+  }
+
+  showAttributesInfo(entity: EntityModel){
+    let entityWithFullInfo: EntityModel;
+    this.equipmentService.single.getById(entity._id == null? 0: entity._id).subscribe(res => {
+      entityWithFullInfo = res;
+      this.tabs.push(entityWithFullInfo);
+      this.fullNames[this.tabs.length] = entityWithFullInfo.warehouseman?.lastName + ' ' + entityWithFullInfo.warehouseman?.firstName + ' ' + entityWithFullInfo.warehouseman?.secondName
+    })
+    this.currentTab = this.tabs.length;
+    // setTimeout(() => {
+    //   this.currentTab = this.tabs.length
+    //   if(this.targets.length > 0)
+    //   {
+    //     const target: ViewContainerRef = this.targets.toArray()[this.tabs.length - 1];
+    //     this.componentRef = target.createComponent(Test1Component);
+    //   }
+    // }, 1)
+  }
+
+}
