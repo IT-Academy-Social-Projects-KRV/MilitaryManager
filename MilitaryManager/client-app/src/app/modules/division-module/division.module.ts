1b2c42d9a3436ab3f5798f1ceeaf537d700ca84d
Natalie <90128567+NataliaHerus@users.noreply.github.com>
2022-10-28 09:16:20 +0300

    Feat/72/update division data (#55)
    * added editing of division
    
    * removed unused ng-model
    
    * removed spelling mistake
    
    * added opportunity to edit subdivisions

--- MilitaryManager/client-app/src/app/modules/division-module/division.module.ts
+++ MilitaryManager/client-app/src/app/modules/division-module/division.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DivisionListComponent } from './pages/division-list/division-list.component';
-import { DivisionEditComponent } from './pages/division-edit/division-edit.component';
 import { RouterModule } from '@angular/router';
 import { PrimeNgComponentsModule } from '../primeng-components-module/primeng-components.module';
 import { DivisionNewComponent } from './pages/division-new/division-new.component';
@@ -9,7 +8,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     DivisionListComponent,
-    DivisionEditComponent,
     DivisionNewComponent
   ],
   imports: [
@@ -19,7 +17,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     RouterModule.forChild([
         { path: '', redirectTo: 'list', pathMatch: 'full' },
         { path: 'list', component: DivisionListComponent },
-        { path: 'edit', component: DivisionEditComponent },
         { path: 'new', component: DivisionNewComponent},
         { path: '**', redirectTo: 'list' }
       ])
