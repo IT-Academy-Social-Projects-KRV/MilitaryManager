3b5e7f9912efa67c897df96def5b8c157a06ca22
Natalia Herus <natalia.gerus892@gmail.com>
2022-10-30 18:52:07 +0200

    created adding of unit by commander

--- MilitaryManager/client-app/src/app/modules/units-module/units.module.ts
+++ MilitaryManager/client-app/src/app/modules/units-module/units.module.ts
@@ -4,19 +4,25 @@ import { UnitsListComponent } from './pages/units-list/units-list.component';
 import { UnitEditComponent } from './pages/unit-edit/unit-edit.component';
 import { RouterModule } from '@angular/router';
 import { PrimeNgComponentsModule } from '../primeng-components-module/primeng-components.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UnitAddComponent } from './pages/unit-add/unit-add.component';
 
 @NgModule({
   declarations: [
     UnitsListComponent,
-    UnitEditComponent
+    UnitEditComponent,
+    UnitAddComponent
   ],
   imports: [
     CommonModule,
     PrimeNgComponentsModule,
+    FormsModule, 
+    ReactiveFormsModule,
     RouterModule.forChild([
         { path: '', redirectTo: 'list', pathMatch: 'full' },
         { path: 'list', component: UnitsListComponent },
         { path: 'edit', component: UnitEditComponent },
+        { path: 'add', component: UnitAddComponent },
         { path: '**', redirectTo: 'list' }
       ])
   ]
