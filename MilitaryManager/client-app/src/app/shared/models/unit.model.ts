ac2fa7a84c0d0f751d04b8702354af68e77f0098
Ponomarov Oleksii <testdrive819@gmail.com>
2022-10-30 15:11:00 +0200

    add parent into unit.model

--- MilitaryManager/client-app/src/app/shared/models/unit.model.ts
+++ MilitaryManager/client-app/src/app/shared/models/unit.model.ts
@@ -1,4 +1,4 @@
-import { BaseModel } from "./base.model";
+import {BaseModel} from "./base.model";
 import {ProfileModel} from "./profile.model";
 import {UnitToEquipmentModel} from "./unitToEquipment.model";
 import {DivisionModel} from "./division.model";
@@ -6,42 +6,45 @@ import {RankModel} from "./rank.model";
 import {PositionModel} from "./position.model";
 
 export class UnitModel extends BaseModel {
-  public lastName: string | null = null;
-  public secondName: string | null = null;
-  public label: string | null = null;
-  public firstName: string | null = null;
-  public divisionId: number | null = null;
-  public rankId: number | null = null;
-  public positionId: number | null = null;
-  public parentId: number | null = null;
+  // public lastName: string | null = null;
+  // public secondName: string | null = null;
+  // public label: string | null = null;
+  // public firstName: string | null = null;
+  // public divisionId: number | null = null;
+  // public rankId: number | null = null;
+  // public positionId: number | null = null;
+  // public parentId: number | null = null;
+  //
+  // public division: DivisionModel | null = null;
+  // public rank: RankModel | null = null;
+  // public parent: UnitModel | null = null;
+  // public position: PositionModel | null = null;
+  //
+  // public subUnits: UnitModel[] | null = null;
 
-  public division: DivisionModel | null = null;
-  public rank: RankModel | null = null;
-  public parent: UnitModel | null = null;
-  public position: PositionModel | null = null;
 
-  public subUnits: UnitModel[] | null = null;
+  // public unitToEquipments: UnitToEquipmentModel[] | null = null;
 
-
-  public profiles: ProfileModel[] | null = null;
-  public unitToEquipments: UnitToEquipmentModel[] | null = null;
-
-  public division: DivisionModel | null = null;
 
   constructor(
-    Id:number|null=null,
-    public lastName:string | null=null,
-    public firstName: string| null=null,
-    public secondName: string| null=null,
-    public divisionId:number|null=null,
+    Id: number | null = null,
+    public lastName: string | null = null,
+    public firstName: string | null = null,
+    public secondName: string | null = null,
+    public divisionId: number | null = null,
     public rankId: number | null = null,
     public rank: string | null = null,
     public positionId: number | null = null,
     public position: string | null = null,
     public parentId: number | null = null,
-    public profiles: ProfileModel[]|null = null,
-    ){
+    public profiles: ProfileModel[] | null = null,
+    public label: string | null = null,
+    public division: DivisionModel | null = null,
+    public parent: UnitModel | null = null,
+    public subUnits: UnitModel[] | null = null,
+    public unitToEquipments: UnitToEquipmentModel[] | null = null
+  ) {
     super(Id);
-}
+  }
 
 }
