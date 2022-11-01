a85fad521fce019afe3f53391f8be6317c44b8c6
Zhykovskiy <68905616+Zhykovskiy@users.noreply.github.com>
2022-11-01 21:58:10 +0200

    Feat/71/update soldier data (#64)
    * Test update profile
    
    * Fix med card
    
    * Remove med card reactive form
    
    * Moved update medCard and personalInfo in 1 method
    
    * Refactor
    
    * Fix bad fix of merge conflict

--- MilitaryManager/client-app/src/app/modules/units-module/pages/unit-info/unit-info.component.ts
+++ MilitaryManager/client-app/src/app/modules/units-module/pages/unit-info/unit-info.component.ts
@@ -5,25 +5,58 @@ import {UnitsService} from "../../../../shared/services/api/unit.service";
 import {
   logExperimentalWarnings
 } from "@angular-devkit/build-angular/src/builders/browser-esbuild/experimental-warnings";
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { AttributeModel } from 'src/app/shared/models/attribute.model';
+import { ApiService } from 'src/app/shared/services/api/api.service';
+import { ProfileModel } from 'src/app/shared/models/profile.model';
 
+
+type Attribute = {
+  name: string | any,
+  value: string
+}
 
 @Component({
   selector: 'app-unit-info',
   templateUrl: './unit-info.component.html',
   styleUrls: ['./unit-info.component.scss']
 })
+
 export class UnitInfoComponent implements OnInit, OnChanges {
 
   @Input() idChild1: number;
 
   unit: UnitModel = new UnitModel();
+  unitInfo: UnitModel = new UnitModel();
   parentFullName: string = "";
   divisionName: string = "";
 
-  constructor(private unitsService: UnitsService) {
+  personalFile: Attribute[] = [];
+
+  isReadonly: boolean = true;
+  attributes: AttributeModel[] = [];
+
+  personalInfoForm: FormGroup = this._formBuilder.group({
+    LastName: ['', Validators.required],
+    FirstName: ['', Validators.required],
+    SecondName: ['', Validators.required],
+    Rank: ['', Validators.required],
+    Position: ['', Validators.required],
+    Division: ['', Validators.required],
+    ParentName: ['', Validators.required]
+  })
+
+  constructor(private unitsService: UnitsService,
+              private _formBuilder: FormBuilder,
+              private messageService: MessageService,
+              private _apiService: ApiService) {
   }
 
   ngOnInit(): void {
+    this._apiService.attributes.collection.getAll().subscribe(attributes => {
+      this.attributes = attributes;
+    })
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -35,6 +68,15 @@ export class UnitInfoComponent implements OnInit, OnChanges {
       .subscribe((u) => {
         this.unit = u;
 
+        this.personalFile=[]
+
+        this.unit.profiles.forEach((profile) =>{
+          this.personalFile.push({
+            name:profile.name,
+            value:profile.value
+          })
+        }) 
+
         if (this.unit.parent != null) {
           this.parentFullName = `${this.unit.parent.lastName} ${this.unit.parent.firstName} ${this.unit.parent.secondName}`;
         } else {
@@ -42,6 +84,41 @@ export class UnitInfoComponent implements OnInit, OnChanges {
         }
 
         this.divisionName = this.unit.division.name;
+
+        this.personalInfoForm.get("LastName").setValue(this.unit.lastName);
+        this.personalInfoForm.get("FirstName").setValue(this.unit.firstName);
+        this.personalInfoForm.get("SecondName").setValue(this.unit.secondName);
+        this.personalInfoForm.get("Rank").setValue(this.unit.rank);
+        this.personalInfoForm.get("Position").setValue(this.unit.position);
+        this.personalInfoForm.get("Division").setValue(this.unit.division.name);
+        if(this.unit.parent!){
+          this.personalInfoForm.get("ParentName").setValue(this.unit.parent.lastName + " " + this.unit.parent.firstName + " " + this.unit.parent.secondName);
+        }
+        else{
+          this.personalInfoForm.get("ParentName").setValue("Немає");
+        }
+
+        
       });
+  }
+
+  edit(){
+    this.isReadonly = false;
+  }
+
+  save() {
+    this.isReadonly = true;
+    this.unit.firstName = this.personalInfoForm.get("FirstName").value;
+    this.unit.lastName = this.personalInfoForm.get("LastName").value;
+    this.unit.secondName = this.personalInfoForm.get("SecondName").value;
+    for(let i = 0; i < this.unit.profiles.length; i++) {
+      this.unit.profiles[i] = new ProfileModel(this.unit.profiles[i].id,
+        this.attributes.find(x => x.name==this.unit.profiles[i].name)?.id, this.unit.id,
+        this.personalFile[i].value, null, this.personalFile[i].name)
+    }
+    this.unitsService.single.update(this.unit).subscribe(
+      () =>  this.messageService.add({ severity: 'success', summary: 'Солдата відредаговано' }),
+      () => this.messageService.add({ severity: 'error', summary: 'Виникла помилка!'})
+    );
   }
 }
