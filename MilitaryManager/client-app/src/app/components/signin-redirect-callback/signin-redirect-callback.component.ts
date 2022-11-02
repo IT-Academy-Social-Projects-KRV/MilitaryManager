8a242c8076b7b73aa9c6b1dd30f52dec3e5f4b1b
Natalie <90128567+NataliaHerus@users.noreply.github.com>
2022-11-02 17:31:05 +0200

    Fix/add check on redirect and guards (#67)
    * fixed login routing, added guards
    
    * removed guard from divisions

--- MilitaryManager/client-app/src/app/components/signin-redirect-callback/signin-redirect-callback.component.ts
+++ MilitaryManager/client-app/src/app/components/signin-redirect-callback/signin-redirect-callback.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/shared/services/api/api.service';
+import { IdentityService } from 'src/app/shared/services/api/identity.service';
 import { UnitUserService } from 'src/app/shared/services/api/unit-user.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
@@ -11,21 +12,29 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class SigninRedirectCallbackComponent implements OnInit {
 
-  constructor(private _authService: AuthService, private _router: Router, private unitUser: UnitUserService) { }
+  constructor(private _authService: AuthService, private _router: Router, private unitUser: UnitUserService,
+    private _identityService: IdentityService) { }
 
   ngOnInit(): void {
     this._authService.finishLogin()
       .then(user => {
-        this.unitUser.GetUnitUser(user.profile.sub)
-          .subscribe(res => {
-            if(res){
-              this._router.navigate(['/'], { replaceUrl: true });
-            }
-            else{
-              this._router.navigateByUrl('finishRegistration');
-            }
+        this._identityService.single.get().subscribe((data: any) => {
+          if (data["0"] == "Admin") {
+            this._router.navigate(['/'], { replaceUrl: true });
           }
-          )
+          else {
+            this.unitUser.GetUnitUser(user.profile.sub)
+              .subscribe(res => {
+                if (res) {
+                  this._router.navigate(['/'], { replaceUrl: true });
+                }
+                else {
+                  this._router.navigateByUrl('finishRegistration');
+                }
+              }
+              )
+          }
+        })
       })
   }
 }
